f3c2c7e7ecf11fd80465e827fa439f6762f80b30
Ilias Loulakakis <liakos@beupset.gr>
2025-06-03 22:36:01 +0300

    removed unused routes and views

--- resources/views/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <h1>Hello index!</h1>
-    <a href="{{route('posts.all')}}">View Posts</a>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
-})->name('index');
-
-Route::get('/posts', function () {
     return view('posts.all');
 })->name('posts.all');
